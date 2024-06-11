fe7852612aebc7f8c67dc67704a4edffe5cc0a88
Cabavas <cabavas@gmail.com>
2024-06-11 20:30:33 -0300

    CORS resolvido

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server URL")})
 @SpringBootApplication
 public class Application {
 
